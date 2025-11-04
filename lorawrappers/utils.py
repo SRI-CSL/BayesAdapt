87fe9ca2333d47ecea15122878be951d1e2a7f2f
Colin Samplawski <colinsamplawski@gmail.com>
2025-11-04 11:37:01 -0800

    adjust wrapper function so it supports 4 bit quant

--- lorawrappers/utils.py
+++ lorawrappers/utils.py
@@ -1,10 +1,10 @@
-from peft.tuners.lora import LoraLayer, Linear, Linear8bitLt
+from peft.tuners.lora import LoraLayer, Linear, Linear4bit, Linear8bitLt
 
 def wrap_lora_layers(module, wrapper_fn, target_modules=[]):
     for name, child in module.named_children():
         is_target = name in target_modules
         is_lora = isinstance(child, LoraLayer)
-        is_linear = isinstance(child, Linear) or isinstance(child, Linear8bitLt)
+        is_linear = isinstance(child, Linear) or isinstance(child, Linear4bit) or isinstance(child, Linear8bitLt)
         if is_target and is_lora and is_linear:
             module._modules[name] = wrapper_fn(child)
         else:
