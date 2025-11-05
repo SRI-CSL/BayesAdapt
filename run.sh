4a276b1296406088c1e8d8b318062d8a2549c1f8
Colin Samplawski <colinsamplawski@gmail.com>
2025-11-05 11:29:53 -0800

    minor update to run script over moving dir

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #export CUDA_VISIBLE_DEVICES=1 
 SEED=0
-GPU_ID=2
+GPU_ID=7
 MODELFAMILY="Qwen"
 HFMODEL="$MODELFAMILY/Qwen2.5-7B"
-CONFIGNAME="train_scalabl_bb"
+CONFIGNAME="train_scalabl"
 #CONFIGNAME="default"
 #WANDBPROJECT="BLoB-Qwen2.5-7B"
 WANDBPROJECT="$CONFIGNAME-Qwen2.5-7B"
@@ -17,10 +17,11 @@ echo $LOGDIR
     #HFMODEL="$MODELFAMILY/$model"
 #done
 
-#python run/train.py --config-name $CONFIGNAME hf_model=$HFMODEL seed=$SEED gpu_id=$GPU_ID\
-    #wandb.project=$WANDBPROJECT \
-    #wandb.name="$WANDBPROJECT-seed${SEED}-hydra" \
-    #logdir=$LOGDIR \
-    #dataset.name=$DATASET
+python run/train.py --config-name $CONFIGNAME hf_model=$HFMODEL seed=$SEED gpu_id=$GPU_ID\
+    wandb.project=$WANDBPROJECT \
+    wandb.name="$WANDBPROJECT-seed${SEED}-hydra" \
+    logdir=$LOGDIR \
+    dataset.name=$DATASET \
+    optim.batch_size=2
 
 python run/evaluate.py --config-name $CONFIGNAME hf_model=$HFMODEL seed=$SEED gpu_id=$GPU_ID logdir=$LOGDIR dataset.name=$DATASET 
