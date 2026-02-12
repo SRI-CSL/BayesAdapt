6f14de868c193849f39a231b456e68358f841e47
Colin Samplawski <colinsamplawski@gmail.com>
2026-02-12 07:45:26 -0800

    updates for active learning and logic dataset

--- active_learn.py
+++ active_learn.py
@@ -1,3 +1,4 @@
+import sys
 import math
 import json
 import torch
@@ -15,22 +16,34 @@ import hydra
 from hydra.utils import instantiate
 from datasets import concatenate_datasets
 from bayesadapt.utils import average_log_probs
+from accelerate.utils import set_seed
+import torch.nn.functional as F
 
+def bald(logits):
+    logprobs = F.log_softmax(logits, dim=-1) #B x N x C
+    probs = F.softmax(logits, dim=-1) #B x N x C
+    avg_probs = probs.mean(dim=1) #B x C
+    log_avg_probs = average_log_probs(logits) #B x C
+    entropy_avg = -torch.sum(avg_probs * log_avg_probs, dim=-1) #B
+    sample_entropy = -torch.sum(probs * logprobs, dim=-1).mean(dim=1) #B
+    bald_score = entropy_avg - sample_entropy
+    return bald_score
 
 @hydra.main(config_path="./conf", config_name="default", version_base=None)
 def main(cfg):
     print(cfg)
-    pool_dataset = SRQA(split='train')
+    set_seed(cfg.seed)
+    pool_dataset = instantiate(cfg.train_dataset, split='train')
     pool_ids = pool_dataset.data['question_id']
-    test_dataset = SRQA(split='test')
+    test_dataset = instantiate(cfg.test_dataset, split='test')
     id_key = 'question_id'
-    initial_train_size = 10
+    num_select = 10
     initial_train_ids = np.random.choice(
         pool_ids, 
-        size=initial_train_size, 
+        size=num_select,
         replace=False
     ).tolist()
-    train_dataset = SRQA(split='train')
+    train_dataset = instantiate(cfg.train_dataset, split='train')
     train_dataset.data = pool_dataset.data.filter(lambda x: x[id_key] in initial_train_ids)
     pool_dataset.data = pool_dataset.data.filter(lambda x: x[id_key] not in initial_train_ids)
 
@@ -55,34 +68,62 @@ def main(cfg):
     # train_dataset = ARC(split='train', difficulty='challenge')
     # train_dataset.data = pool_dataset.data.filter(lambda x: x[id_key] in initial_train_ids)
     # pool_dataset.data = pool_dataset.data.filter(lambda x: x[id_key] not in initial_train_ids)
-
-    num_select = 10
-    for i in range(100):
+    history = []
+    for i in range(20):
         trainer = instantiate(cfg.trainer, cfg=cfg) #cold start each time
-        trainer.update_dataloaders(train_dataset=train_dataset, test_dataset=pool_dataset)
+        evaldir = trainer.evaldir.replace('id', 'active_learn')
+
+        json_path = os.path.join(evaldir, 'results.json')
+        if os.path.exists(json_path):
+            if not cfg.overwrite:
+                sys.exit(f"Results already exist at {json_path}, experiment already complete")
+
+    
+        
+        pool_ids = pool_dataset.data[id_key]
+        #randomly sample 1000 from pool for efficiency, can be changed to all if desired
+        if len(pool_ids) > 1000:
+            sampled_pool_ids = np.random.choice(pool_ids, size=1000, replace=False).tolist()
+            sampled_pool_dataset = instantiate(cfg.train_dataset, split='train')
+            sampled_pool_dataset.data = pool_dataset.data.filter(lambda x: x[id_key] in sampled_pool_ids)
+        else:
+            sampled_pool_dataset = pool_dataset
+
+        trainer.update_dataloaders(train_dataset=train_dataset, test_dataset=sampled_pool_dataset)
         trainer.train(save=False, use_wandb=False)
         pool_metrics, logits_dict = trainer.evaluate(save=False, verbose=False)
         
         logit_ids = list(logits_dict.keys())
-        logits = torch.stack([logits_dict[qid] for qid in logit_ids]) #B x C
+        logits = torch.stack([logits_dict[qid] for qid in logit_ids]) #B x N x C
+        logits = logits.to(torch.float64)
+        B, num_samples, num_classes = logits.shape
 
-        logprobs = average_log_probs(logits) #B x C
-        num_classes = logprobs.shape[1]
-        probs = torch.exp(logprobs)
-        entropies = -torch.sum(probs * logprobs, dim=1) / math.log(num_classes)
+        if num_samples == 1: #only one sample, probably MLE, so use predictive entropy instead of BALD
+            logprobs = average_log_probs(logits) #B x C
+            probs = torch.exp(logprobs)
+            scores = -torch.sum(probs * logprobs, dim=1) / math.log(num_classes) #entropy normalized by log(num_classes)
+        else:
+            scores = bald(logits) #B
 
-        top_indices = torch.topk(
-            entropies,
+        top_values, top_indices = torch.topk(
+            scores,
             k=num_select,
             largest=True
-        ).indices.tolist()
+        )
 
-        selected_qids = [logit_ids[idx] for idx in top_indices]
+        selected_qids = [logit_ids[idx] for idx in top_indices.tolist()]
         selected_hf = pool_dataset.data.filter(lambda x: x[id_key] in selected_qids)
         
         #test on true test set for plots later
         trainer.update_dataloaders(test_dataset=test_dataset)
         test_metrics, _ = trainer.evaluate(save=False)
+
+        history.append({
+            'selected_qids': selected_qids,
+            'pool_metrics': pool_metrics,
+            'test_metrics': test_metrics,
+            'top_scores': top_values.cpu().tolist(),
+        })
 
         # selection_probs = entropies / entropies.sum()
         # selected_qids = torch.multinomial(
@@ -93,7 +134,13 @@ def main(cfg):
         # selected_hf = pool_dataset.data.filter(lambda x: x[id_key] in selected_qids)
         train_dataset.data = concatenate_datasets([train_dataset.data, selected_hf])
         pool_dataset.data = pool_dataset.data.filter(lambda x: x[id_key] not in selected_qids)
-    import ipdb; ipdb.set_trace() # noqa
+    
+    if not os.path.exists(evaldir):
+        os.makedirs(evaldir, exist_ok=True)
+
+    json_path = os.path.join(evaldir, 'results.json')
+    with open(json_path, 'w') as f:
+        json.dump(history, f)
 
 
 
--- bayesadapt/datasets/circuit_logic.py
+++ bayesadapt/datasets/circuit_logic.py
@@ -1,11 +1,12 @@
 from datasets import load_dataset
 from torch.utils.data import Dataset, DataLoader
 import reasoning_gym
+from datasets import Dataset as HFDataset
 
 prompt_template = "Consider the following logical expression: {expression}\nWhat is the truth value of this expression given the following variable assignments?\n"
 class CircuitLogic(Dataset):
     labels = ['0', '1']
-    def __init__(self, split='train'):
+    def __init__(self, split='train', representation='expression'):
         if split not in ['train', 'validation', 'test']:
             raise ValueError(f"Unknown split: {split}")
 
@@ -16,16 +17,44 @@ class CircuitLogic(Dataset):
         else:  # test
             data = reasoning_gym.create_dataset('circuit_logic', size=1000, seed=44)
 
+        # expression_legend = {
+            # '&': 'AND',
+            # '↑': 'NAND',
+            # '⊕': 'XOR',
+            # "'": 'Negate',
+            # "+": 'OR'
+        # }
+        # expression_legend = "\n".join([f"{k}: {v}" for k, v in expression_legend.items()])
+        # expression_legend = f"Legend:\n{expression_legend}".strip()
 
         self.data = []
         for i, x in enumerate(data):
+            question = x['question']
+            question_tokens = question.split('\n\n')
+            header, gate, legend, assignments, final_question = question_tokens
+            legend = legend.replace(' for gates', '').strip()
+            if representation == 'expression':
+                logic = x['metadata']['expression']
+
+                #use same symbols as in gates, for slightly more fair ood eval
+                logic = logic.replace('&', '&&')
+                logic = logic.replace('↑', '↑↑')
+                logic = logic.replace('⊕', '⊕⊕')
+                logic = logic.replace("'", ">o")
+                logic = logic.replace('+', '++')
+                header = header.replace('circuit', 'expression')
+            elif representation == 'circuit':
+                logic = gate.strip()
+            else:
+                raise ValueError(f"Unknown representation: {representation}")
+            prompt = f"{header}\n{logic}\n{legend}\n{assignments}\n{final_question}"
             self.data.append({
-                'expression': x['metadata']['expression'],
-                'assignments': x['metadata']['assignments'],
+                'prompt': prompt,
                 'question_id': f"{split}_{i}",
                 'answer': x['answer']
             })
 
+        self.data = HFDataset.from_list(self.data)
 
 
 
@@ -35,14 +64,14 @@ class CircuitLogic(Dataset):
     def __getitem__(self, idx):
         item = self.data[idx]
 
-        prompt = prompt_template.format(expression=item['expression'])
-        for var, val in item['assignments'].items():
-            prompt += f"{var} = {val}\n"
+        # prompt = prompt_template.format(expression=item['expression'])
+        # for var, val in item['assignments'].items():
+            # prompt += f"{var} = {val}\n"
 
-        prompt += "Output 0 or 1 only."
+        # prompt += "Output 0 or 1 only."
 
         return {
-            'prompt': prompt.strip(),
+            'prompt': item['prompt'].strip(),
             'label': self.labels.index(item['answer']),
             'question_id': item['question_id']
         }
