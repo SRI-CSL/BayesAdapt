883f8dedf0c73fc026ee101237aaf9be3ba2abe4
Colin Samplawski <csamplawski@cs.umass.edu>
2026-02-22 10:06:24 -0800

    add script

---
+++ scripts/tempscale/vlm_sweep.sh
@@ -0,0 +1,17 @@
+python train_and_evaluate.py --multirun \
+    hydra/launcher=ray \
+    +hydra.launcher.ray.init.num_gpus=4 \
+    +hydra.launcher.ray.remote.num_gpus=1 \
+    +lora=default \
+    +lora/wrapper=tempscale \
+    lora.config.r=8 \
+    optim.train_split=validation \
+    lora.load_mle_checkpoint=True \
+    lora.requires_grad=False \
+    optim.nll_optimizer.lr=1e-3 \
+    hf_model=Qwen/Qwen3-VL-2B-Instruct,Qwen/Qwen3-VL-4B-Instruct,Qwen/Qwen3-VL-8B-Instruct \
+    dataset@train_dataset=mmstar,slake,MathVerse \
+    collate_fn=vlm\
+    seed=0,1,2,3\
+    pbar=True \
+    gpu_id=0 #ray will handle CUDA_VISIBLE_DEVICES so we just set gpu_id=0 here
